4a5f1f9ae134eeec92fe51ef0298bc09d13c2fed
bellaratmelia <bella.ratmelia@gmail.com>
2014-12-26 11:34:47 +0800

    make login page prettier

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -1,24 +1,39 @@
-<html>
-	<head>	
-		<title>Login Page</title>
-	</head>
-	<body>
-		{{ Form::open(['route' => 'sessions.store']) }}	
+<!DOCTYPE html>
+<html lang="en">
 
-			<div>
-			 	{{ Form::label('email','Email: ') }}
-			 	{{ Form::email('email') }}
-			</div>
+<head>
+  @include('layouts.dashboard.headerdash')
+</head>
 
-			<div>
-			 	{{ Form::label('password','Password: ') }}
-			 	{{ Form::password('password') }}
-			</div>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        {{ Form::open(array('route' => 'sessions.store')) }}
+                            <fieldset>
+                                <div class="form-group">
+									 {{ Form::email('email', '', array('placeholder' => 'Your Email', 'class' => 'form-control input-md')) }}
+                                    
+                                </div>
+                                <div class="form-group">									
+                                	{{ Form::password('password', array('placeholder' => 'Your Password', 'class' => 'form-control input-md')) }}
+                                </div>
+                                {{ Form::submit('Login', array('class' => 'btn btn-lg btn-success btn-block')) }}
+                            </fieldset>
+                        {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ <!-- /#wrapper -->
+@include('layouts.dashboard.footerdash')
+</body>
 
-			<div>
-			 	{{ Form::submit('Login') }}
-			</div>
-
-		{{ Form::close() }}
-	</body>
 </html>
+
