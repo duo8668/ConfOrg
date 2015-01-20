d0dafd482ae4b08536488344f44d8bc80e970a11
pjng003 <pohjun.ng@gmail.com>
2015-01-20 17:06:46 +0800

    update by pohjun 20/1/2015

--- app/models/userrole/User.php
+++ app/models/userrole/User.php
@@ -6,60 +6,58 @@ use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
-
+	use UserTrait, RemindableTrait;
 	protected $table = 'users';
 	protected $primaryKey = 'user_id';
 
-	protected $fillable = ['title','firstname','lastname','email','password','remember_token'];
+	protected $fillable = ['firstname','lastname','email','email_temp','password','password_temp','remember_token','code','active','created_at','updated_at'];
 
 	protected $guarded = array('user_id');
 
-	public $timestamps = false;
-
-	use UserTrait, RemindableTrait;
-	public $errors;
-	protected $hidden = array('password', 'remember_token');
+	
+	// public $errors;
+	// protected $hidden = array('password', 'remember_token');
 
 
 
-	public static $rules = 
-	[
-	'Email' => 'required',
-	'Password' => 'required'
-	];
+	// public static $rules = 
+	// [
+	// 'Email' => 'required',
+	// 'Password' => 'required'
+	// ];
 
 	
-	public function isValid($data)
-	{
-		$validation = Validator::make($data, static::$rules);
+	// public function isValid($data)
+	// {
+	// 	$validation = Validator::make($data, static::$rules);
 
-		if($validation->passes()) return true;
+	// 	if($validation->passes()) return true;
 
-		$this->errors = $validation->messages();
-		return false;
-	}
+	// 	$this->errors = $validation->messages();
+	// 	return false;
+	// }
 	
-	public function roles()
-	{
-		return $this->belongsToMany('Role', 'ConfUserRole');
-	}
+	// public function roles()
+	// {
+	// 	return $this->belongsToMany('Role', 'ConfUserRole');
+	// }
 
-	public function hasRole($name)
-	{
-		foreach ($this->roles as $role)
-		{
-			if ($role->rolename == $name) return true;
-		}
-		return false;
-	}
+	// public function hasRole($name)
+	// {
+	// 	foreach ($this->roles as $role)
+	// 	{
+	// 		if ($role->rolename == $name) return true;
+	// 	}
+	// 	return false;
+	// }
 
-	public static function IsInRole($name,$confId){
-		$userid = Session::get('userid');
-		//$confid = '1';
-		//Auth::user()->user_id
-		$user = Auth::user();
+	// public static function IsInRole($name,$confId){
+	// 	$userid = Session::get('userid');
+	// 	//$confid = '1';
+	// 	//Auth::user()->user_id
+	// 	$user = Auth::user();
 
-		return $user->hasRole($name);
-	}
+	// 	return $user->hasRole($name);
+	// }
 
 }
