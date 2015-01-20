24f49cdb406869eac9a9426916aa2f22c17ea46f
bellaratmelia <bella.ratmelia@gmail.com>
2015-01-20 22:53:39 +0800

    prettifying login + forgot password page

--- app/views/users/forget.blade.php
+++ app/views/users/forget.blade.php
@@ -1,14 +1,45 @@
- <form action = "{{URL::route('users-forget-password-post')}}" method = "post">
- 	
- 	<div class = "field">
- 	Email: <input type ="email" name="email" {{Input::old('email') ? ' value="' . e(Input::old('email')) . '"' : ''  }}>
-	@if($errors->has('email'))
-	 		{{ $errors->first('email')	}}
-	 	@endif
- 	</div>
- 	<input type="submit" value="Recover">
- 	{{ Form::token() }}
- </form>
-@if(Session::has('message'))
-    {{Session::get('message')}} 
-@endif
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  @include('layouts.dashboard.headerdash')
+</head>
+
+<body>
+   <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-green">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Recover Password</h3>
+                    </div>
+                    <div class="panel-body">
+                        @if(Session::has('message'))
+                            {{Session::get('message')}} 
+                        @endif
+                        {{ Form::open(array('route' => 'users-forget-password-post', 'method' => 'post')) }}
+                            <fieldset>
+                                <div class="form-group">
+                                    <input type ="email" class="form-control" placeholder="E-mail" name="email" {{Input::old('email') ? ' value="' . e(Input::old('email')) . '"' : ''  }} autofocus>
+                                    @if($errors->has('email'))
+                                        {{  $errors->first('email')  }} 
+                                    @endif                                     
+                                </div>
+                                
+                                <!-- Button -->
+                                 <input type="submit" class="btn btn-md btn-success btn-block" value="Recover Password">
+                                {{ Form::token() }} 
+                                 <hr>
+                                <a href="{{ URL::route('users-sign-in') }}" class="pull-right">Back to Login</a>
+                            </fieldset>
+                        {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+ @include('layouts.dashboard.footerdash')
+
+</body>
+</html>
--- app/views/users/signin.blade.php
+++ app/views/users/signin.blade.php
@@ -1,30 +1,54 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  @include('layouts.dashboard.headerdash')
+</head>
+
 <body>
-                        <form action = "{{URL::route('users-sign-in-post')}}" method = "post">
-                            <div class = "field">
-                            Email: <input type ="text" name="email" {{Input::old('email') ? ' value="' . e(Input::old('email')) . '"' : ''  }}>
-                                @if($errors->has('email'))
-                                    {{  $errors->first('email')  }} 
-                                @endif     
-                            </div>
+   <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-green">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        @if(Session::has('message'))
+                            {{Session::get('message')}} 
+                        @endif
+                        {{ Form::open(array('route' => 'users-sign-in-post', 'method' => 'post')) }}
+                            <fieldset>
+                                <div class="form-group">
+                                    <input type ="email" class="form-control" placeholder="E-mail" name="email" {{Input::old('email') ? ' value="' . e(Input::old('email')) . '"' : ''  }} autofocus>
+                                    @if($errors->has('email'))
+                                        {{  $errors->first('email')  }} 
+                                    @endif                                     
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control" placeholder="Password" name="password">
+                                    @if($errors->has('password'))
+                                        {{  $errors->first('password')  }} 
+                                    @endif                                    
+                                </div>
+                                <div class="checkbox">
+                                    <label>{{ Form::checkbox('remember', 'remember') }} Remember Me</label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                 <input type="submit" class="btn btn-md btn-success btn-block" value="Sign In">
+                                {{ Form::token() }} 
+                                <hr>
+                                <a href="{{ URL::route('users-forget-password') }}" class="pull-right">Forget Password?</a>
+                            </fieldset>
+                        {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                            <div class = "field">
-                                Password: <input type="password" name="password">
-                                @if($errors->has('password'))
-                                    {{  $errors->first('password')  }} 
-                                @endif         
-                            </div> 
-                            <div>
-                              {{Form::checkbox('remember', 'remember')}}
-                              {{ Form::label('remember', 'Remember Me')}}
-                            </div>                                
-                            <input type="submit" value="Sign In">
-                            {{ Form::token() }}                       
-                        </form>
-            <a href="{{ URL::route('users-forget-password') }}">Forget Password?</a></li>
-@if(Session::has('message'))
-    {{Session::get('message')}} 
-@endif
+ @include('layouts.dashboard.footerdash')
+
 </body>
-
 </html>
+
