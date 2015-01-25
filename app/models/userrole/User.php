4b58ddada1f87ef8b1fee7f01b71fefa60445df2
pjng003 <pohjun.ng@gmail.com>
2015-01-25 12:19:49 +0800

    updated user by pohjun 25/1/2015

--- app/models/userrole/User.php
+++ app/models/userrole/User.php
@@ -14,7 +14,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	protected $fillable = ['firstname','lastname','email','email_temp','password','password_temp','remember_token','code','active','created_at','updated_at'];
 	protected $guarded = array('user_id');
 
-public function isInRole($confid,$rolename)
+public function hasConfRole($confid,$rolename)
  {
   $role_id = DB::table('roles')
   ->where('rolename','=',$rolename)
@@ -32,7 +32,7 @@ public function isInRole($confid,$rolename)
 
 
  }
- public function hasPermission($confid,$permissionname){
+ public function hasConfPermission($confid,$permissionname){
     $permission_id = DB::table('permissions')
     ->where('permission_name','=',$permissionname)
     ->lists('permission_id');
@@ -60,5 +60,46 @@ public function isInRole($confid,$rolename)
    return false;
  }
 
- 
+ public function hasSysRole($rolename){
+  $role_id = DB::table('roles')
+  ->where('rolename','=',$rolename)
+  ->lists('role_id');
+
+  $userid= Auth::user()->user_id;
+
+  $role = DB::table('sysrole')
+  ->where('user_id','=',$userid)
+  ->where('role_id','=',$role_id)
+  ->first();
+
+  return ($role != null);
+
+ }
+
+ public function hasSysPermission($permissionname){
+    $permission_id = DB::table('permissions')
+    ->where('permission_name','=',$permissionname)
+    ->lists('permission_id');
+
+    $list_of_role_that_has_permission =  
+    DB::table('role_permission')
+    ->where('permission_id','=',$permission_id)
+    ->first();
+
+    $userid= Auth::user()->user_id;
+
+   $lists_of_role_user_is = 
+   DB::table('sysrole')
+    ->where('user_id','=',$userid)
+    ->get();
+
+  foreach ($lists_of_role_user_is as $lists_of_role_user_is)
+  {
+   
+      if($lists_of_role_user_is->role_id == $list_of_role_that_has_permission->role_id)
+       return true;
+  }
+  
+   return false;
+ }
 }
