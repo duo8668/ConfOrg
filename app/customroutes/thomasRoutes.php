b68930fd8e8a63637bfb01d620ac71082bebe9ca
ThomasRalee <thomas.leera@gmail.com>
2015-01-29 01:46:54 +0800

    excel export

--- app/customroutes/thomasRoutes.php
+++ app/customroutes/thomasRoutes.php
@@ -2,22 +2,24 @@
 
 // Route::get('/', 'ThomasController@home');
 
-// Route::get('/venue', 'ThomasController@venue');
+//Route::get('/venue', 'ThomasController@venue');
 // Route::post('/venue', array('as' => 'venue', 'uses'=>'ThomasController@venue'));
 // Route::get('/venue2', 'ThomasController@venue2');
 // Route::post('/venue2', array('as' => 'venue2', 'uses'=>'ThomasController@venue2'));
 // Route::get('/venue2/test', 'ThomasController@test');
 
-Route::get('/about', array('as' => 'about', 'uses' => 'ThomasController@about'));
+Route::get('/about', 'ThomasController@about');
+//Route::post('/about', array('as' => 'about', 'uses'=>'ThomasController@test'));
+
+Route::post('download', array('uses' => 'ThomasController@download'));
+Route::post('previewMap', array('uses' => 'ThomasController@previewMap'));
+Route::post('import', array('uses' => 'ThomasController@import'));
+
 Route::resource('venue', 'ThomasController');
 Route::resource('equipmentcategory', 'EquipmentCategoryController');
 Route::resource('equipment', 'EquipmentController');
 Route::resource('room', 'RoomController');
 
-
-
-//Route::get('foo/bar', 'FooController@about');
-//Route::resource('foo', 'FooController');
 
 
 
