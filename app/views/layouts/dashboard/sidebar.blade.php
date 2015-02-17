230d96134be0812b48a7fc4c5745a35a26164d8a
bellaratmelia <bella.ratmelia@gmail.com>
2015-02-17 14:20:51 +0800

    - edited sidebar, added invoice and import, change venue

--- app/views/layouts/dashboard/sidebar.blade.php
+++ app/views/layouts/dashboard/sidebar.blade.php
@@ -18,24 +18,24 @@
                         <!-- USER COMMON SETTINGS -->
                         <li><a href="{{ url('dashboard') }}"><i class="fa fa-dashboard fa-fw"></i> Main Dashboard</a></li>
                         <li><a href="{{URL::route('users-profile', ['email' => Auth::user()->email])}}"><i class="fa fa-user fa-fw"></i> User Profile</a></li>
-                        <li class="sidebar-divider"></li>
+                        <!-- <li class="sidebar-divider"></li> -->
 
 
                         <!-- PARTICIPANT SETTINGS, appear when user == participant -->
-                       <!--  <li><a href="#"><i class="fa fa-cc-paypal fa-fw"></i> Make Payment</a></li>
+                        <!-- <li><a href="#"><i class="fa fa-cc-paypal fa-fw"></i> Make Payment</a></li> -->
                         <li><a href="#"><i class="fa fa-ticket fa-fw"></i> Invoice & Confirmation</a></li>
-                        <li><a href="#"><i class="fa fa-envelope-o fa-fw"></i> Contact Conference Staff</a></li>
-                        <li class="sidebar-divider"></li> -->
+                        <!-- <li><a href="#"><i class="fa fa-envelope-o fa-fw"></i> Contact Conference Staff</a></li> -->
+                        <li class="sidebar-divider"></li>
 
 
                         <!-- SUBMISSION LINKS, appear when viewing conference currently calling for papers -->
                         <li><a href="{{ URL::route('submission.index') }}"><i class="fa fa-file fa-fw"></i> Your Submissions</a></li>
-                        <li><a href="{{ URL::route('submission.create') }}"><i class="fa fa-plus fa-fw"></i> New Submission</a></li>
+                        <!-- <li><a href="{{ URL::route('submission.create') }}"><i class="fa fa-plus fa-fw"></i> New Submission</a></li> -->
                         <li class="sidebar-divider"></li>
 
 
                         <!-- REVIEW LINKS, appear if user == reviewers -->
-                        <li><a href="{{ URL::route('reviews.index') }}"><i class="fa fa-comment fa-fw"></i> Submission Reviews</a>
+                        <li><a href="{{ URL::route('reviews.index') }}"><i class="fa fa-comment fa-fw"></i> Your Reviews</a>
                         </li>
                         <li><a href="{{ URL::route('review.topics') }}"><i class="fa fa-check fa-fw"></i> Set Preferred Topics</a>
                         </li>
@@ -52,30 +52,13 @@
 
 
                         <!-- VENUE MANAGEMENT LINKS, appear if user == Provider/Facilitator -->
-                        <li><a href="#"><i class="fa fa-map-marker fa-fw"></i> Venue<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="{{ URL::route('venue.index') }}">My Venues</a>
-                                </li>
-                                <li>
-                                    <a href="{{ URL::route('venue.create') }}">Add New Venue</a>
-                                </li>
-                                <li>
-                                    <a href="{{ URL::route('equipmentcategory.index') }}">Category</a>
-                                </li>
-                                <li>
-                                    <a href="{{ URL::route('equipment.index') }}">Equipment</a>
-                                </li>
-                                <li>
-                                    <a href="{{ URL::route('room.index') }}">Room</a>
-                                </li>
-                                
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>                     
-                        <li><a href="#"><i class="fa fa-cutlery fa-fw"></i>Food</a></li>   
-                        <li><a href="#"><i class="fa fa-smile-o fa-fw"></i>Entertainment</a></li>
-                         <li class="sidebar-divider"></li>
+                        <li><a href="{{ URL::route('venue.index') }}"><i class="fa fa-map-marker fa-fw"></i> Venue</a></li>
+                        <!-- <li><a href="{{ URL::route('venue.create') }}">Add New Venue</a></li> -->
+                        <li><a href="{{ URL::route('equipmentcategory.index') }}"><i class="fa fa-sitemap fa-fw"></i> Category</a></li>
+                        <li><a href="{{ URL::route('equipment.index') }}"><i class="fa fa-cogs fa-fw"></i> Equipment</a></li>
+                        <li><a href="{{ URL::route('room.index') }}"><i class="fa fa-location-arrow fa-fw"></i> Room</a></li>     
+                        <li><a href="{{ URL::to('/import') }}"><i class="fa fa-upload fa-fw"></i> Import</a></li>                    
+                        <li class="sidebar-divider"></li>
 
                         <!-- END OF SIDEBAR MENU -->
                     </ul>
