e06016f4a27eaa83ea153e00781636adec871eb7
bellaratmelia <bella.ratmelia@gmail.com>
2015-02-21 23:41:51 +0800

    - applied css to the invite pages

--- app/views/admins/invite_resource.blade.php
+++ app/views/admins/invite_resource.blade.php
@@ -1,34 +1,68 @@
- <form action = "{{URL::route('admins-invite-resource-post')}}" method = "post">
+@extends('layouts.dashboard.master')
+@section('page-header')
+Invite a Resource Provider
+@stop
+@section('content')
+<!-- BREADCRUMB -->
+<ol class="breadcrumb">
+  <li><a href="{{ URL::to('/dashboard') }}">Dashboard</a></li>
+  <li class="active">Invite a Resource Provider</li>    
+</ol>
+<hr>
+<div class="row">
+	<div class="col-md-12">
+  	{{ Form::open(array('route' => 'admins-invite-resource-post', 'class' => 'form-horizontal','method' => 'POST')) }}
  	
- 	<div class = "field">
- 	Resource Provider's Email: <input type ="email" name = "email">
-	 	@if($errors->has('email'))
-	 		{{ $errors->first('email')	}}
-	 	@endif
- 	</div>
+	 	<legend>Invite Resource Provider Company</legend>
+	 	<div class="form-group @if ($errors->has('email')) has-error @endif">
+	      {{ Form::label('email', 'Resource Provider Email', ['class' => 'col-sm-2 control-label']) }}    
+	      <div class="col-sm-6">     
+	        {{ Form::email('email', null, array('class' => 'form-control', 'required' => 'required')) }}
+	      </div>
+	    	@if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
+	    </div>
+	    <div class="clearfix"></div>
+
+	    <div class="form-group @if ($errors->has('company')) has-error @endif">
+	      {{ Form::label('company', 'Resource Provider Email', ['class' => 'col-sm-2 control-label']) }}    
+	      <div class="col-sm-6">   
+	       {{ Form::select('company', $company_options, null, ['class' => 'form-control', 'required' => 'required']) }}
+	       <p class="help-block">If the company name is not listed here, please add it in first in the form below</p>
+	      </div>
+	    	@if ($errors->has('company')) <p class="help-block">{{ $errors->first('company') }}</p> @endif
+	    </div>
+	    <div class="clearfix"></div>
+	    <div style="margin-bottom:20px;"
+	 	<div class="row">  
+	      <div class="col-md-6 col-md-offset-2">
+	        <!-- Button -->     
+	        {{ Form::submit('Send Invite', array('class' => 'btn btn-primary btn-md')) }}
+	      </div>
+	    </div>
+ 	{{ Form::close() }} 
+ 	 <div style="margin-bottom:40px;"
+ 	{{ Form::open(array('route' => 'admins-add-company-post', 'class' => 'form-horizontal','method' => 'POST')) }}
+		<legend>Add New Company</legend>
+
+ 		<div class="form-group @if ($errors->has('new')) has-error @endif">
+	      {{ Form::label('new', 'Company Name', ['class' => 'col-sm-2 control-label']) }}    
+	      <div class="col-sm-6">     
+	        {{ Form::text('new', null, array('class' => 'form-control', 'required' => 'required')) }}
+	      </div>
+	    	@if ($errors->has('new')) <p class="help-block">{{ $errors->first('new') }}</p> @endif
+	    </div>
+	    <div class="clearfix"></div>
+ 		 <div style="margin-bottom:20px;"
+ 		<div class="row">  
+	      <div class="col-md-6 col-md-offset-2">
+	        <!-- Button -->     
+	        {{ Form::submit('Add Company', array('class' => 'btn btn-primary btn-md')) }}
+	      </div>
+	    </div>
+ 	{{ Form::close() }} 
  	
-
-	<div class = "field">
- 	Resource Provider's Company: 
- 	{{ Form::select('company', $company_options) }}
-	 	@if($errors->has('company'))
-	 		{{ $errors->first('company')	}}
-	 	@endif
  	</div>
+</div>
 
- 	<input type="submit" value="Invite!">
- 	{{ Form::token() }}
- </form>
 
-<form action = "{{URL::route('admins-add-company-post')}}" method = "post">
- 	<div class = "field">
- 	Add in new Company: : <input type ="text" name = "new">
-	 	
- 
- <input type="submit" value="Add!">
- @if($errors->has('new'))
-	 		{{ $errors->first('new')	}}
-	 	@endif
- 	{{ Form::token() }}
- 	</div>
-</form>
+@stop
--- app/views/users/invite.blade.php
+++ app/views/users/invite.blade.php
@@ -1,26 +1,55 @@
- <form action = "{{URL::route('users-invite-friend-post')}}" method = "post">
- 	
- 	<div class = "field">
- 	Friend's Email: <input type ="email" name = "email">
-	 	@if($errors->has('email'))
-	 		{{ $errors->first('email')	}}
-	 	@endif
- 	</div>
- 	
-	<div class = "field">
- 	Friend's First Name: <input type ="text" name = "firstname">
-	 	@if($errors->has('firstname'))
-	 		{{ $errors->first('firstname')	}}
-	 	@endif
- 	</div>
+@extends('layouts.dashboard.master')
+@section('page-header')
+Invite a Friend to use ORAFER
+@stop
+@section('content')
+<!-- BREADCRUMB -->
+<ol class="breadcrumb">
+  <li><a href="{{ URL::to('/dashboard') }}">Dashboard</a></li>
+  <li class="active">Invite a Friend</li>    
+</ol>
+<hr>
+<div class="row">
+  {{ Form::open(array('route' => 'users-invite-friend-post', 'class' => 'form-horizontal','method' => 'POST')) }}
+  <div class="col-md-12">
 
-	<div class = "field">
- 	Friend's Last Name: <input type ="text" name = "lastname">
-	 	@if($errors->has('lastname'))
-	 		{{ $errors->first('lastname')	}}
-	 	@endif
- 	</div>
+ 	<div class="form-group @if ($errors->has('email')) has-error @endif">
+      {{ Form::label('email', 'Friend Email', ['class' => 'col-sm-2 control-label']) }}    
+      <div class="col-sm-6">     
+        {{ Form::email('email', null, array('class' => 'form-control', 'required' => 'required')) }}
+      </div>
+    @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
+    </div>
+    <div class="clearfix"></div>
 
- 	<input type="submit" value="Invite!">
- 	{{ Form::token() }}
- </form>
+    <div class="form-group @if ($errors->has('firstname')) has-error @endif">
+      {{ Form::label('firstname', 'Friend First Name', ['class' => 'col-sm-2 control-label']) }}    
+      <div class="col-sm-6">     
+        {{ Form::text('firstname', null, array('class' => 'form-control', 'required' => 'required')) }}
+      </div>
+    @if ($errors->has('firstname')) <p class="help-block">{{ $errors->first('firstname') }}</p> @endif
+    </div>
+    <div class="clearfix"></div>
+
+    <div class="form-group @if ($errors->has('email')) has-error @endif">
+      {{ Form::label('email', 'Friend Last Name', ['class' => 'col-sm-2 control-label']) }}    
+      <div class="col-sm-6">     
+        {{ Form::text('email', null, array('class' => 'form-control', 'required' => 'required')) }}
+      </div>
+    @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
+    </div>
+    <div class="clearfix"></div>
+
+ 	<hr>
+    
+    <div class="row">  
+      <div class="col-md-6 col-md-offset-2">
+        <!-- Button -->     
+        {{ Form::submit('Send Invite', array('class' => 'btn btn-primary btn-md')) }}
+      </div>
+    </div> 
+ </div>
+  {{ Form::close() }} 
+</div>
+
+@stop
