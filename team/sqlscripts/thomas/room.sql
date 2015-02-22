b8dff4c4fdcdcc10b5fed0a5e7a9dbd07457e202
ThomasRalee <thomas.leera@gmail.com>
2015-02-23 01:30:03 +0800

    added Availability to the room

--- team/sqlscripts/thomas/room.sql
+++ team/sqlscripts/thomas/room.sql
@@ -1,8 +1,3 @@
-
--- ----------------------------
--- Table structure for room
--- ----------------------------
-DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `room_id` int(11) NOT NULL AUTO_INCREMENT,
   `venue_id` int(11) NOT NULL,
@@ -10,12 +5,9 @@ CREATE TABLE `room` (
   `capacity` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `rental_cost` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_by` int(11) NOT NULL,
-  `modified_by` int(11) NULL,
+  `modified_by` int(11) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `avaliable` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'yes',
   PRIMARY KEY (`room_id`,`venue_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of room
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
